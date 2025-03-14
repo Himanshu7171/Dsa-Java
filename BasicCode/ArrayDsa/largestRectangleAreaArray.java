be54bc84af6ec8ec41aa167f7bda4d0ee6a465c5
Himanshu7171 <himanshulahaneh3@gmail.com>
2025-03-14 23:50:33 +0530

    add files

---
+++ BasicCode/ArrayDsa/largestRectangleAreaArray.java
@@ -0,0 +1,30 @@
+package BasicCode.ArrayDsa;
+
+public class largestRectangleAreaArray {
+    public  static int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] stack = new int[n]; // Manual stack to store indices
+        int top = -1;  // Pointer for stack
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i]; // Treat extra 0 at end
+
+            while (top != -1 && currHeight < heights[stack[top]]) {
+                int height = heights[stack[top--]]; // Pop height
+                int width = (top == -1) ? i : i - stack[top] - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            stack[++top] = i; // Push index manually
+        }
+
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        int height [] = {2,1,5,6,2,3} ;
+        
+         int result =  largestRectangleArea(height);
+         System.out.println(result);
+    }
+}
