07965c6803075378cc6ba4513c2506f6b0851daf
Himanshu7171 <himanshulahaneh3@gmail.com>
2025-03-31 23:49:00 +0530

    add files

---
+++ BasicCode/ArrayDsa/maxprofit.java
@@ -0,0 +1,25 @@
+
+package  BasicCode.ArrayDsa;
+
+public class maxprofit {
+    public  int maxProfit(int[] prices) {
+        int maxProfit = 0;
+         
+         for (int i = 1; i < prices.length; i++) {
+             if (prices[i] > prices[i - 1]) {
+                 maxProfit += prices[i] - prices[i - 1];
+             }
+         }
+         
+         return maxProfit;
+     }
+     public static void main(String[] args) {
+        maxprofit dl = new maxprofit();
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        
+        System.out.println("Maximum Profit: " + dl.maxProfit(prices));
+    
+     }
+}
